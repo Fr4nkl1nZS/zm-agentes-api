e0ecbd32fc2dd657b6b99b91478bb74388bb9894
Franklin <apostolfranklin1@gmail.com>
2026-06-22 17:40:49 -0500

    Configurar CORS para zmdeportes.com x2

--- api.py
+++ api.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
@@ -10,10 +10,12 @@ app.add_middleware(
     allow_origins=[
         "https://zmdeportes.com",  # Permitir la URL de tu sitio web
         "http://zmdeportes.com",  # Permitir la URL de tu sitio web
+        "https://www.zmdeportes.com",  # Permitir la URL de tu sitio web
         "*"
         ],  # Permitir todas las URLs de origen
         allow_methods=["*"],  # Permitir todos los métodos HTTP
         allow_headers=["*"],  # Permitir todos los encabezados
+        allow_credentials=True,  # Permitir el envío de cookies y credenciales
 )
 
 class Mensaje(BaseModel):
